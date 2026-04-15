3fb86ac6537692efb8a964eed4c51a793c6e990d
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2026-04-15 15:39:46 +0530

    [Anuj Chaturvedi] add. Random module and its function in python

---
+++ Practice_problems/random_and_function.py
@@ -0,0 +1,6 @@
+import random
+
+#random module is used to generate random numbers
+print(random.randint(1, 10)) # generates a random integer between 1 and 10
+print(random.random()) # generates a random float between 0 and 1
+print(random.choice(["apple", "banana", "cherry"])) # randomly selects an item from the list
