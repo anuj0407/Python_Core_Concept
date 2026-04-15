5ad61586227dd5a6fcdfab164fbcd3b796453692
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2026-04-15 15:41:53 +0530

    [Anuj Chaturvedi] add. Math module and its function in python

---
+++ Practice_problems/math_and_function.py
@@ -0,0 +1,9 @@
+import math
+# math module provides mathematical functions
+print(math.sqrt(16)) # calculates the square root of 16
+print(math.pow(2, 3)) # calculates 2 raised to the power of 3
+print(math.pi) # prints the value of pi
+print(math.factorial(5)) # calculates the factorial of 5
+print(math.gcd(48, 18)) # calculates the greatest common divisor of 48 and 18
+
+#math module also provides functions for trigonometric calculations as well as other mathematical operations.
