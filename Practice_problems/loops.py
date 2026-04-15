f9a4a968f30b534ead50334f569f96f837e493b2
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2026-04-15 15:45:04 +0530

    [Anuj Chaturvedi] add. Loops : for loop and while loop

---
+++ Practice_problems/loops.py
@@ -0,0 +1,15 @@
+#loop
+#for loop
+print("for loop iterations:")
+for i in range(5):
+    print(i)
+
+#while loop
+count = 0
+while count < 5:
+    count += 1
+
+print("While loop count:", count)
+
+
+
