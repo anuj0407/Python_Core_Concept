b420b6e40518821dc8f292020382c463a80cd9e2
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2026-04-15 15:50:14 +0530

    [Anuj Chaturvedi] add. Arthemetic operators usage

---
+++ Practice_problems/operators.py
@@ -0,0 +1,6 @@
+number_1 = int(input("Enter number 1: "))
+number_2 = int(input("Enter number 2: "))
+print(f"{number_1} + {number_2} = {number_1+number_2}")
+print(f"{number_1} - {number_2} = {number_1-number_2}")
+print(f"{number_1} * {number_2} = {number_1*number_2}")
+print(f"{number_1} / {number_2} = {number_1/number_2}")
