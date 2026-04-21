5ec7a851c1747db0b448ab953c95da1a3affa1a3
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2026-04-21 14:37:49 +0530

    [Anuj Chaturvedi] add. Inheritance principle of OOPs

---
+++ inherit_class.py
@@ -0,0 +1,23 @@
+# Inheritance is one of the OOPs principle 
+# Inheritance is a concept that allows a new class (called a child class or subclass) to inherit attributes and methods from an existing class (called a parent class or superclass).
+# The child class can also have its own attributes and methods, and it can override the methods of the parent class.
+
+#parent class
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        print(f"Animal Name: {self.name}")
+
+    def sound(self):
+        print("Some generic sound")
+
+#child class
+class Dog(Animal):
+    def sound(self): #overriding the sound method of the parent class
+        print(f"{self.name} barks!") 
+
+dog1 = Dog("Bob")
+dog1.info() # parent class method
+dog1.sound() # child class overridden method
