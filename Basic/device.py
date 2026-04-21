96d47a9935740f76e80393bbcb352e20a2e4bc39
Anuj Chaturvedi <114936919+anuj0407@users.noreply.github.com>
2026-04-21 15:42:28 +0530

    [Anuj Chaturvedi] add. Dunder method

--- Basic/device.py
+++ Basic/device.py
@@ -1,7 +1,7 @@
 # Dunder method is a special method in Python that starts and ends with double underscores (__) and is used to define the behavior of an object when certain operations are performed on it. Dunder methods are also known as magic methods or special methods.
 # __init__ is a dunder method that is called when an object is created from a class. It is used to initialize the attributes of the object.
 
-class Device:
+class Device: 
     def __init__(self,name):
         print("init called for: ", name)
         self.name = name.upper()
